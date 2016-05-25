3645342ca30ccca62a0989ebc6147f435d7624f9
WilliamBalbino <williambalbino14@hotmail.com>
2016-05-25 16:13:47 -0300

    classe tarefa

--- To_Do/app/src/main/java/com/example/william/to_do/Tarefa.java
+++ To_Do/app/src/main/java/com/example/william/to_do/Tarefa.java
@@ -1,7 +1,47 @@
 package com.example.william.to_do;
 
+import java.util.Date;
+import java.util.Timer;
+
 /**
  * Created by willi on 20/05/2016.
  */
 public class Tarefa {
+
+    private String titulo;
+    private String descricao;
+    private Date data;
+    private Timer hora;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Timer getHora() {
+        return hora;
+    }
+
+    public void setHora(Timer hora) {
+        this.hora = hora;
+    }
 }
--- To_Do/app/src/main/java/com/example/william/to_do/TarefaAdapter.java
+++ To_Do/app/src/main/java/com/example/william/to_do/TarefaAdapter.java
@@ -4,4 +4,6 @@ package com.example.william.to_do;
  * Created by willi on 20/05/2016.
  */
 public class TarefaAdapter {
+
+    
 }
