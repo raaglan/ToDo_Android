f17178c2b3e16b2907d7e3376c29c3265759ea13
raaglan <raglanpb@gmail.com>
2016-05-25 17:10:09 -0300

    variaveis e campos da tela novaActivity

--- To_Do/app/src/main/java/com/example/william/to_do/NovaTarefaActivity.java
+++ To_Do/app/src/main/java/com/example/william/to_do/NovaTarefaActivity.java
@@ -2,12 +2,68 @@ package com.example.william.to_do;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class NovaTarefaActivity extends AppCompatActivity {
+
+    private TextView titulo;
+    private TextView descricao;
+    private DatePicker data;
+    private TimePicker hora;
+    private Button buttonOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nova_tarefa);
+
+
+
+        titulo = (TextView)findViewById(R.id.editTitulo);
+        titulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        descricao = (TextView)findViewById(R.id.editDescricao);
+        descricao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        data = (DatePicker)findViewById(R.id.datePicker);
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        hora = (TimePicker)findViewById(R.id.timePicker);
+        hora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonOK = (Button)findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
+
